9b1594424fdd9167ad706ab0e82b769b2b214d8a
paperwiz <hancdh@gmail.com>
2021-03-24 02:36:46 +0900

    heroku deployment

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://paperwiz:!ehdgurdl12@cluster0.q8ihu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+};
--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://paperwiz:!ehdgurdl12@cluster0.q8ihu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
-}).then(() => console.log("MongoDB Connected..."))
-.catch(err => console.log(err))
+// application /x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!1111111");
+});
+
+app.post("/register", (req, res) => {
+  // 회원 가입할때 필요한 정보들을 clint에서 가져오면
+  // 그것을 DB에 넣어준다
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
